da628369095ee49e4d536d13c66397bd38bad084
Zak Stern <zak.stern@gmail.com>
2013-07-27 15:00:00 -0700

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
-  root :to => 'static_pages#home'
+  get "users/new"
 
+  root :to => 'static_pages#home'
+  match '/static_pages/home', :to =>'static_pages#home'
+  match '/signup',  to: 'users#new',            via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
