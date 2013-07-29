eb207c792d9e82fc4d40cd2b9529a5c94c4f1c4c
Zak Stern <zak.stern@gmail.com>
2013-07-28 23:53:50 -0700

    create a sessions controller

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "AuthenticationPages" do
+
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
+  end
+
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+    end
+  end
+end
